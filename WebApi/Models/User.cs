6c3bd1d9deb1fa33da8a70929c2d4f5a00443e77
yeheng.ma <Myh_00@outlook.com>
2024-04-01 17:57:26 +1300

    SQL Server connection

--- WebApi/Models/User.cs
+++ WebApi/Models/User.cs
@@ -2,7 +2,7 @@ namespace WebApi.Models
 {
     public class User
     {
-        public int UId { get; set; }
+        public int Id { get; set; }
         public string Email { get; set; }
         public string EncryptedPwd { get; set; }
         public string FirstName { get; set; }
