e35afeebb90c1573177f27e9ebcadfff31d62f1e
yeheng.ma <Myh_00@outlook.com>
2024-04-23 10:43:27 +1200

    cache pages

--- web/src/services/jobService.js
+++ web/src/services/jobService.js
@@ -1,17 +1,18 @@
 export const fetchJobs = async (page, pageSize = 10) => {
+  const key = `jobs-${page}`;
+  const cachedJobs = sessionStorage.getItem(key);
   const endpoint = `${process.env.REACT_APP_API_BASE_URL}/job?page=${page}&pagesize=${pageSize}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
   try {
-    const response = await fetch(endpoint, options);
+    if (cachedJobs) {
+      return JSON.parse(cachedJobs);
+    }
+    const response = await fetch(endpoint);
     if (!response.ok) {
       throw new Error("Failed to fetch jobs");
     }
-    return await response.json();
+    const data = await response.json();
+    sessionStorage.setItem(key, JSON.stringify(data));
+    return data;
   } catch (error) {
     console.error("Error fetching jobs:", error);
     throw error;
